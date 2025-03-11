bf9d79a35357e9fd242efa0a723a29881ffce5a3
Shreya Hegde <shreyahegde@Shreyas-MacBook-Pro.local>
2025-03-11 19:50:53 +0530

    Fixed compilation issues in main.cpp

---
+++ main/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, Jenkins!" << std::endl;
+
+    // Add, commit, and push the file (This was incorrectly formatted earlier)
+    
+    return 0;
+}
